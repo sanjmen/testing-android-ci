ead25023429cd4f3da40adf47464f457f63789a4
Angel Freire <cuerty@gmail.com>
2015-09-18 18:46:45 -0300

    Test.

--- app/src/main/java/com/cuerty/androidtraviscitest/MainActivity.java
+++ app/src/main/java/com/cuerty/androidtraviscitest/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBody = (TextView) findViewById(R.id.body);
+
+        mBody.setText("Hola mundo!");
     }
 
     @Override
