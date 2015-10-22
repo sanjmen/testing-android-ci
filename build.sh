c1ffae3361ff3c79412b77d7544227a92ada6758
Santiago Mendez <sanjmen@gmail.com>
2015-10-22 17:58:05 -0300

    add tests gradle tasks

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 
 ./gradlew connectedCheck
 if [ $? -ne 0 ]; then exit 1; fi
-./gradlew build;
+./gradlew domain:test
+if [ $? -ne 0 ]; then exit 1; fi
+./gradlew data:test
+if [ $? -ne 0 ]; then exit 1; fi
+./gradlew app:generateDebugSources;
+if [ $? -ne 0 ]; then exit 1; fi
+./gradlew app:generateDebugAndroidTestSources;
+if [ $? -ne 0 ]; then exit 1; fi
+./gradlew app:assembleDebug;
 if [ $? -ne 0 ]; then exit 1; fi
